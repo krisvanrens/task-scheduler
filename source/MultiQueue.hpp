da8a9e45a78e0f27582bb9651f46c6eba27055e4
Kris van Rens <krisvanrens@gmail.com>
2022-06-19 17:23:14 +0200

    FIX: Off-by-one error.

--- source/MultiQueue.hpp
+++ source/MultiQueue.hpp
@@ -9,7 +9,7 @@ constexpr std::size_t MAX_NUMBER_OF_QUEUES = 1024;
 constexpr std::size_t MAX_QUEUE_LENGTH     = 8192;
 
 template<typename T, std::size_t MaxQueueLength>
-requires(MaxQueueLength < MAX_QUEUE_LENGTH) class MultiQueue final {
+requires(MaxQueueLength <= MAX_QUEUE_LENGTH) class MultiQueue final {
   using Queue = std::queue<T>;
 
   std::vector<Queue> queues_;
