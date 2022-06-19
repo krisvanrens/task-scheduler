a279523911bcbd20321e3dd8030103c21a8d3e5b
Kris van Rens <krisvanrens@gmail.com>
2022-06-19 17:06:30 +0200

    RFT: Define constant.

--- source/MultiQueue.hpp
+++ source/MultiQueue.hpp
@@ -5,9 +5,10 @@
 #include <stdexcept>
 
 constexpr std::size_t MAX_NUMBER_OF_QUEUES = 1024;
+constexpr std::size_t MAX_QUEUE_LENGTH     = 8192;
 
 template<typename T, std::size_t MaxQueueLength>
-requires(MaxQueueLength < 8192) class MultiQueue final {
+requires(MaxQueueLength < MAX_QUEUE_LENGTH) class MultiQueue final {
   using Queue = std::queue<T>;
 
   std::vector<Queue> queues_;
