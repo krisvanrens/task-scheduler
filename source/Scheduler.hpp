e3ad8eb069c7d3d0f9094702a0de4c8480d1cd63
Kris van Rens <krisvanrens@gmail.com>
2022-06-23 23:48:13 +0200

    CHG: Make the scheduler use the multiqueue.

--- source/Scheduler.hpp
+++ source/Scheduler.hpp
@@ -3,35 +3,35 @@
 #include <condition_variable>
 #include <functional>
 #include <mutex>
-#include <queue>
 #include <stdexcept>
 #include <thread>
 #include <vector>
 
+#include "Multiqueue.hpp"
+
 template<typename Task, unsigned int MaxQueueLength>
 requires(MaxQueueLength < 8192) class Scheduler final {
-  std::queue<Task>          queue_;
-  const unsigned int        num_executors_;
-  std::vector<std::jthread> executors_;
+  const unsigned int               num_executors_;
+  Multiqueue<Task, MaxQueueLength> queue_;
+  std::vector<std::jthread>        executors_;
 
-  void executor(std::stop_token stop_token) {
+  void executor(std::stop_token stop_token, unsigned int id) {
     while (!stop_token.stop_requested()) {
-      if (!queue_.empty()) {
-        queue_.front()();
-        queue_.pop();
-      }
+      // TODO
+      queue_.pop(id).value_or([] {})();
     }
   }
 
   void create_executors() {
     for (unsigned int i = 0; i < num_executors_; i++) {
-      executors_.emplace_back(std::bind_front(&Scheduler<Task, MaxQueueLength>::executor, this));
+      executors_.emplace_back(std::bind_front(&Scheduler<Task, MaxQueueLength>::executor, this), i);
     }
   }
 
 public:
   Scheduler(unsigned int num_executors)
-    : num_executors_{num_executors} {
+    : num_executors_{num_executors}
+    , queue_{num_executors} {
     if (num_executors_ == 0) {
       throw std::underflow_error("At least one executor must be requested");
     }
@@ -46,12 +46,7 @@ public:
   Scheduler(Scheduler&&) noexcept            = default;
   Scheduler& operator=(Scheduler&&) noexcept = default;
 
-  [[nodiscard]] bool schedule(Task&& task) noexcept {
-    if (queue_.size() < MaxQueueLength) {
-      queue_.push(std::move(task));
-      return true;
-    }
-
-    return false;
+  [[nodiscard]] bool schedule(Task&& task) {
+    return queue_.push(std::move(task));
   }
 };
--- tests/Scheduler.cpp
+++ tests/Scheduler.cpp
@@ -16,24 +16,33 @@ TEST_SUITE("Scheduler") {
     Job t2{[] { printf("t2!\n"); }};
     Job t3{[] { printf("t3!\n"); }};
     Job t4{[] { printf("t4!\n"); }};
+    Job t5{[] { printf("t5!\n"); }};
+    Job t6{[] { printf("t6!\n"); }};
+    Job t7{[] { printf("t7!\n"); }};
 
     REQUIRE(t1);
     REQUIRE(t2);
     REQUIRE(t3);
     REQUIRE(t4);
 
-    Scheduler<Job, 3> s1{1};
+    Scheduler<Job, 3> s1{2};
 
     CHECK(s1.schedule(std::move(t1)));
     CHECK(s1.schedule(std::move(t2)));
     CHECK(s1.schedule(std::move(t3)));
+    CHECK(s1.schedule(std::move(t4)));
+    CHECK(s1.schedule(std::move(t5)));
+    CHECK(s1.schedule(std::move(t6)));
 
     CHECK(!t1);
     CHECK(!t2);
     CHECK(!t3);
+    CHECK(!t4);
+    CHECK(!t5);
+    CHECK(!t6);
 
-    CHECK(!s1.schedule(std::move(t4)));
-    CHECK(t4);
+    CHECK(!s1.schedule(std::move(t7)));
+    CHECK(t7);
 
     CHECK_THROWS_AS((Scheduler<Job, 3>{100}), std::overflow_error);
   }
