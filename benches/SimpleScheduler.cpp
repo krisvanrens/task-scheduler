ffe24f990223bc2eb0fc5b955da02eede1a23916
Kris van Rens <krisvanrens@gmail.com>
2022-07-08 20:56:51 +0100

    ADD: Scheduler benchmark.

--- benches/SimpleScheduler.cpp
+++ benches/SimpleScheduler.cpp
@@ -4,7 +4,9 @@
 
 #include <cstddef>
 
-using Scheduler = SimpleScheduler<100>;
+static constexpr std::size_t QUEUE_LENGTH = 100;
+
+using Scheduler = SimpleScheduler<QUEUE_LENGTH>;
 
 static void BM_Construction(benchmark::State& state) {
   for (auto _ : state) {
@@ -12,6 +14,20 @@ static void BM_Construction(benchmark::State& state) {
   }
 }
 
+static void BM_ScheduleWork(benchmark::State& state) {
+  Scheduler x{static_cast<std::size_t>(state.range(0))};
+  const auto schedule_tasks = [&x]{
+    for (unsigned int i = 0; i < QUEUE_LENGTH; i++) {
+      [[maybe_unused]] auto result = x.schedule([]{});
+    }
+  };
+
+  for (auto _ : state) {
+    schedule_tasks();
+  }
+}
+
 BENCHMARK(BM_Construction)->RangeMultiplier(2)->Range(1, 16);
+BENCHMARK(BM_ScheduleWork)->RangeMultiplier(2)->Range(1, 16);
 
 BENCHMARK_MAIN();
